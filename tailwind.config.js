a87a3d9a742397f314889c29a385b9f4670be7b8
IlerioDef <47086473+IlerioDef@users.noreply.github.com>
2023-04-20 20:22:42 -0400

    Tailwind CSS Tutorial #5 - Tailwind Config - DONE

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './public/**/*.{html,js}',
+    content: ['./public/**/*.{html,js}',
+    './components/**/*.{html,js}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary:'#FF6363',
+        secondary:{
+          100: '#e2e2d5',
+          200: '#888883',
+        }
+      }
+    },
   },
   plugins: [],
 }
+
